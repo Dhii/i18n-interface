2918f138de60982982a0bedaf9d9048a76513973
Anton Ukhanev <xedin.unknown@gmail.com>
2017-03-12 19:05:29 +0100

    Made a more gradual and SoC interface hierarchy

--- src/TranslatorInterface.php
+++ src/TranslatorInterface.php
@@ -2,34 +2,24 @@
 
 namespace Dhii\I18n;
 
-use Dhii\Util\String\StringableInterface as Stringable;
-use ArrayAccess;
-use Dhii\Data\ValueAwareInterface;
-use Dhii\I18n\Exception\TranslationExceptionInterface;
-use Dhii\I18n\Exception\I18nExceptionInterface;
-
 /**
- * Something that can act as a string translator.
+ * Something that can represent a translator, which can translate a subject.
  *
- * @since 0.1
+ * @since [*next-version*]
  */
 interface TranslatorInterface
 {
     /**
-     * Translates a string.
-     *
-     * If values are given, MUST attempt to use them for substituting placeholders in the string.
+     * Translates a subject.
      *
      * @since 0.1
      *
-     * @param string|Stringable               $string  The string to translate.
-     * @param array|ArrayAccess|null          $params  A map of values to use for placeholder substitution, if any.
-     * @param string|ValueAwareInterface|null $context A context for the string, if any.
+     * @param mixed $subject The subject to translate.
      *
      * @throws TranslationExceptionInterface If could not translate string.
      * @throws I18nExceptionInterface        If a problem occurs that is not directly related to the translation process.
      *
-     * @return string The translated string, possibly with placeholders substituted.
+     * @return mixed The translated subject.
      */
-    public function translate($string, $params = null, $context = null);
+    public function translate($subject);
 }
