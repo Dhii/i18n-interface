9148f1d3ff0a6bf9436338cf2a50ca5172343d73
Anton Ukhanev <xedin.unknown@gmail.com>
2017-03-12 19:26:45 +0100

    Next version placeholders replaced again

--- src/TranslatorInterface.php
+++ src/TranslatorInterface.php
@@ -5,7 +5,7 @@ namespace Dhii\I18n;
 /**
  * Something that can represent a translator, which can translate a subject.
  *
- * @since [*next-version*]
+ * @since 0.1
  */
 interface TranslatorInterface
 {
