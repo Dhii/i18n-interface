9862dc95df0908fd5837f6d6cb7a74d78339b007
Anton Ukhanev <xedin.unknown@gmail.com>
2017-03-12 19:17:22 +0100

    Added test

--- test/functional/FormatTranslatorInterfaceTest.php
+++ test/functional/FormatTranslatorInterfaceTest.php
@@ -45,4 +45,19 @@ class FormatTranslatorInterfaceTest extends TestCase
 
         $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the test subject could not be created');
     }
+
+    /**
+     * Tests arg count and types for `translate()`.
+     *
+     * @since 0.1
+     */
+    public function testTranslate()
+    {
+        $subject = $this->createInstance();
+        $arg1 = 'apple';
+        $arg2 = array('banana');
+        $arg3 = 'strawberry';
+
+        $subject->translate($arg1, $arg2, $arg3);
+    }
 }
